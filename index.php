f8bcb379aaa85d4ffcedfcc330d20135731c122e
Kaique Mendonca <kai_que15@hotmail.com>
2020-03-22 23:36:00 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+        <title> Studio Vanessa Merquiades</title>
+        <link type="text/css" href="style.css" rel="stylesheet"/>
+        <script type="text/javascript" src="script.js"></script>
+    </head>
+
+    <body>
+
+    <header>
+        <div class="logo">
+           <a href="index.php"><img src="assets/vm.png"/> </a>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="index.php" id="home">HOME</a></li>
+                <li><a href="#" >AGENDAR SERVIÇOS</a></li>
+                <li><a href="sobre.php" >QUEM SOMOS</a></li>
+                <li><a href="" >LOCALIZAÇÃO</a></li>
+                <li><a href="#" >CONTATO</a></li>
+            </ul>
+        </div>
+
+    </header>
+
+    <section>
+        <div class="slideshow" id="slideshow">
+            <div class="slideshowarea">
+                <div class="slide" style="background-image:url('assets/foto1.png')"></div>
+                <div class="slide" style="background-image:url('assets/foto2.png')"></div>
+                <div class="slide" style="background-image:url('assets/foto3.png')"></div>
+                <div class="slide" style="background-image:url('assets/foto4.png')"></div>
+            </div>
+
+        </div>
+
+        <div class="valor">
+            <div class="valortexto">
+                <a>O loiro que sempre sonhou, agora você pode ter. </a>
+                
+            </div>
+
+            <div class="btagendar">
+                <a href="">AGENDE AGORA UM DE NOSSOS SERVIÇOS</a>
+                <img src="assets/seta.png"/>
+            </div>
+        </div>
+
+    </section>
+    <footer>
+        <div class="texto">
+            <a>© 2016 - 2020</a>
+            <a>STUDIO VANNESA MERQUIADES</a>
+                <div class="wpp">
+                    <img src="assets/whatsapp.png"/>
+                    <a>(82) 99987-4571</a>
+                </div>
+
+        </div>
+        <div class="icones">
+            <ul>
+                <li><a href="https://www.instagram.com/studiovanessamerquiades/"><img src="assets/instagram.png"/></a></li>
+                <li><a></a></li>
+            </ul>
+        </div>
+
+        <div class="iconelocal">
+            <img src="assets/local.png"/>
+        </div>
+
+        <div class="localizacao">
+            <a href="https://www.google.com/maps/place/Studio+Vanessa+Merquiades/@-9.549769,-35.7464426,15z/data=!4m5!3m4!1s0x0:0xce6f619521ffbd10!8m2!3d-9.549769!4d-35.7464426" id="local">RUA ELITA PINTO QUINTELA (RUA 62 - CJ G RAMOS, TV. GRACILIANO RAMOS, AL, 57073-208)</a>
+            <a>TER-SEX 9H ÀS 19H / SÁB 9H ÀS 17H</a>
+        </div>
+    </footer>
+
+    </body>
+</html>
---
+++ sobre.php
@@ -0,0 +1,68 @@
+html>
+    <head>
+        <meta charset="UTF-8">
+        <meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+        <title> Studio Vanessa Merquiades</title>
+        <link type="text/css" href="style.css" rel="stylesheet"/>
+        <script type="text/javascript" src="script.js"></script>
+    </head>
+
+    <body>
+
+    <header>
+        <div class="logo">
+           <a href="index.php"><img src="assets/vm.png"/> </a>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="index.php" id="homesobre">HOME</a></li>
+                <li><a href="#" >AGENDAR SERVIÇOS</a></li>
+                <li><a href="" id="quemsobre">QUEM SOMOS</a></li>
+                <li><a href="#wrapper1" >LOCALIZAÇÃO</a></li>
+                <li><a href="#" >CONTATO</a></li>
+            </ul>
+        </div>
+
+    </header>
+
+    <div class="sobretexto">
+
+        <div class="sobrefoto">
+            <img src="assets/vanessa.png"/>
+        </div>
+
+        <div class="texto1">
+            <a>Somos Especialistas em cabelos loiros.<br>Especialistas em alinhamento capilar.<br>Utilizamos os melhores produtos para cuidar da melhor forma dos nossos clientes</a>
+        </div>
+
+    </div>
+
+    <footer>
+        <div class="texto">
+            <a>© 2016 - 2020</a>
+            <a>STUDIO VANNESA MERQUIADES</a>
+                <div class="wpp">
+                    <img src="assets/whatsapp.png"/>
+                    <a>(82) 99987-4571</a>
+                </div>
+
+        </div>
+        <div class="icones">
+            <ul>
+                <li><a href="https://www.instagram.com/studiovanessamerquiades/"><img src="assets/instagram.png"/></a></li>
+                <li><a></a></li>
+            </ul>
+        </div>
+
+        <div class="iconelocal">
+            <img src="assets/local.png"/>
+        </div>
+
+        <div class="localizacao">
+            <a href="https://www.google.com/maps/place/Studio+Vanessa+Merquiades/@-9.549769,-35.7464426,15z/data=!4m5!3m4!1s0x0:0xce6f619521ffbd10!8m2!3d-9.549769!4d-35.7464426" id="local">RUA ELITA PINTO QUINTELA (RUA 62 - CJ G RAMOS, TV. GRACILIANO RAMOS, AL, 57073-208)</a>
+            <a>TER-SEX 9H ÀS 19H / SÁB 9H ÀS 17H</a>
+        </div>
+    </footer>
+
+    </body>
+</html>
